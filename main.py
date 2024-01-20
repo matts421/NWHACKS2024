c6ba9e7d069c539e897769b22e16c775adb80823
Aman <apraka01@student.ubc.ca>
2024-01-20 14:20:06 -0800

    Added random trash falling.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pygame
 from trash_bins.bins import AbstractTrashBin
 from trash.trash import AbstractTrash
-import sys
+import random
 
 # Trash traits
 TRASH_SPEED = 5
@@ -21,7 +21,8 @@ class Game:
     clock: pygame.time.Clock
     running: bool
     player: AbstractTrashBin
-    trash: AbstractTrash
+    trash_list: list
+    spawn_timer: int
 
     def start(self):
         pygame.init()
@@ -29,7 +30,8 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
         self.player = AbstractTrashBin(BIN_PATHS["GARBAGE"], "GarbageBin", self)
-        self.trash = AbstractTrash(TRASH_PATHS["TRASH"], "Garbage", self)
+        self.spawn_timer = 0
+        self.trash_list = []        
         self.game_loop()
         pygame.quit()
 
@@ -41,7 +43,18 @@ class Game:
             self.screen.fill("black")
             self.handle_key_events()
             self.player.render()
-            self.trash.render()
+
+            for trash in self.trash_list.copy():
+                trash.fall()
+                trash.render()
+            
+            self.spawn_timer += 1
+            if self.spawn_timer % 50 == 0 and len(self.trash_list) < 10:  # 60 ticks per second, 5 seconds * 60 ticks = 300
+                x_position = random.randint(0, SCREEN_WIDTH - 50)
+                new_trash = AbstractTrash(TRASH_PATHS["TRASH"], "Garbage", self, [x_position, 0])
+                self.trash_list.append(new_trash)
+
+            # self.trash.render()
             pygame.display.flip()
             self.clock.tick(60)
 
@@ -56,58 +69,9 @@ class Game:
             self.player.pos[1] -= 5
         if keys[pygame.K_DOWN]:
             self.player.pos[1] += 5
+        if keys[pygame.K_q]:
+            self.running = False
 
 if __name__ == "__main__":
     game : Game = Game()
     game.start()
-
-# import pygame
-# import sys
-
-# # Initialize Pygame
-# pygame.init()
-
-# # Set up the display
-# width, height = 800, 600
-# screen = pygame.display.set_mode((width, height))
-# pygame.display.set_caption("Pygame Keyboard Input")
-
-# # Set up colors
-# black = (0, 0, 0)
-# white = (255, 255, 255)
-
-# # Set up the player's initial position
-# player_x, player_y = width // 2, height // 2
-# player_speed = 5
-
-# # Main game loop
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-
-#     # Get the state of all keys
-#     keys = pygame.key.get_pressed()
-
-#     # Update the player's position based on key input
-#     if keys[pygame.K_LEFT]:
-#         player_x -= player_speed
-#     if keys[pygame.K_RIGHT]:
-#         player_x += player_speed
-#     if keys[pygame.K_UP]:
-#         player_y -= player_speed
-#     if keys[pygame.K_DOWN]:
-#         player_y += player_speed
-
-#     # Fill the screen with a black background
-#     screen.fill(black)
-
-#     # Draw the player as a white rectangle
-#     pygame.draw.rect(screen, white, (player_x, player_y, 50, 50))
-
-#     # Update the display
-#     pygame.display.flip()
-
-#     # Control the frame rate
-#     pygame.time.Clock().tick(60)
--- trash/trash.py
+++ trash/trash.py
@@ -1,7 +1,7 @@
 import pygame
 from exceptions import InvalidPathError
 ## from main import BIN_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT, Game
-TRASH_SPEED = 5
+TRASH_SPEED = 2
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
@@ -11,15 +11,24 @@ class AbstractTrash:
     pos: list # (x, y) position tuple
     BIN_HEIGHT = 50
 
-    def __init__(self, img_path: str, name: str, game):
+    def __init__(self, img_path: str, name: str, game, pos: list):
         try:
             self.img = pygame.image.load(img_path)
         except IOError:
             raise InvalidPathError("This image file does not exist!")
         
         self.name = name
-        self.pos = [SCREEN_WIDTH // 5, SCREEN_HEIGHT // 5]
+        self.pos = pos
         self.game = game
+
+    def fall(self):
+        # Move the trash down the screen
+        self.pos[1] += TRASH_SPEED
+
+        # Delete the trash if it goes out of bounds
+        if self.pos[1] > SCREEN_HEIGHT:
+            self.game.trash_list.remove(self)
+            del self
 
     def render(self):
         self.game.screen.blit(self.img, self.pos)
