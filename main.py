1687146f61691135a7c14d16b3216fab48693c3c
matthew <matthewasmith442@gmail.com>
2024-01-20 14:18:54 -0800

    added whole lotta stuff

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 import pygame
 from trash_bins.bins import AbstractTrashBin
 from trash.trash import AbstractTrash
-import sys
 
 # Trash traits
 TRASH_SPEED = 5
 TRASH_PATHS = {"TRASH" : "./assets/banana.png"}
+BACKGROUND_IMAGE_PATH = "./assets/6.png"
+FLOOR_IMAGE_PATH = "./assets/floor.png"
+
+FLOOR_IMAGE_WIDTH = 18
+FLOOR_IMAGE_HEIGHT = 36
+FLOOR_SCALE = 4
 
 # Bin traits
 BIN_SPEED = 5
-BIN_PATHS = {"GARBAGE" : "./assets/trash.jpg"}
+BIN_PATHS = {"GARBAGE" : "./assets/trash.png"}
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -20,31 +25,52 @@ class Game:
     screen: pygame.display
     clock: pygame.time.Clock
     running: bool
+    background: pygame.image
     player: AbstractTrashBin
     trash: AbstractTrash
+    floor: pygame.image
 
     def start(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.initialize_assets()
         self.player = AbstractTrashBin(BIN_PATHS["GARBAGE"], "GarbageBin", self)
         self.trash = AbstractTrash(TRASH_PATHS["TRASH"], "Garbage", self)
         self.game_loop()
         pygame.quit()
+
+    def initialize_assets(self):
+        background = pygame.image.load(BACKGROUND_IMAGE_PATH)
+        self.background = pygame.transform.scale(background,
+                                                 (SCREEN_WIDTH, SCREEN_HEIGHT))
+        floor_png = pygame.image.load(FLOOR_IMAGE_PATH)
+        subset_rect = pygame.Rect(91, 16, FLOOR_IMAGE_WIDTH, FLOOR_IMAGE_HEIGHT)
+        floor_png = floor_png.subsurface(subset_rect)
+        self.floor = pygame.transform.scale(floor_png,
+                                            (FLOOR_SCALE * FLOOR_IMAGE_WIDTH, FLOOR_SCALE * FLOOR_IMAGE_HEIGHT))
 
     def game_loop(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            self.screen.fill("black")
+
+
+            self.screen.blit(self.background, (0, 0))
+            self.draw_floor()
+
+            #self.screen.fill("black")
             self.handle_key_events()
             self.player.render()
             self.trash.render()
             pygame.display.flip()
             self.clock.tick(60)
 
+    def draw_floor(self):
+        for i in range(SCREEN_WIDTH // (FLOOR_SCALE * FLOOR_IMAGE_WIDTH) + 1):
+            self.screen.blit(self.floor, (i * FLOOR_SCALE * FLOOR_IMAGE_WIDTH, SCREEN_HEIGHT - 50))
     
     def handle_key_events(self):
         keys = pygame.key.get_pressed()
@@ -52,10 +78,6 @@ class Game:
             self.player.move_left()
         if keys[pygame.K_RIGHT]:
             self.player.move_right()
-        if keys[pygame.K_UP]:
-            self.player.pos[1] -= 5
-        if keys[pygame.K_DOWN]:
-            self.player.pos[1] += 5
 
 if __name__ == "__main__":
     game : Game = Game()
