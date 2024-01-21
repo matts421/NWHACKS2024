b06a0b8a1f131f199664ef964ed5167bde287091
Aman <apraka01@student.ubc.ca>
2024-01-20 17:36:09 -0800

    Added states for trash.

--- trash/trash.py
+++ trash/trash.py
@@ -1,28 +1,42 @@
 import pygame
 from exceptions import InvalidPathError
 ## from main import BIN_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT, Game
-TRASH_SPEED = 5
+TRASH_SPEED = 3
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+GARBAGE_NAME = "GARBAGE"
+PAPER_NAME = "PAPER"
+COMPOST_NAME = "COMPOST"
+GLASS_NAME = "GLASS"
+
+TRASH_NAMES = [GARBAGE_NAME, PAPER_NAME, COMPOST_NAME, GLASS_NAME]
 
 class AbstractTrash:
     name: str
     rect: pygame.Rect
-    color = (255, 0, 0)  # Red color for the square
+    color = (255, 0, 0)
     size = (20, 20)
+    state = int
 
-    def __init__(self, name: str, game, pos: list):
+    def __init__(self, name: str, game, pos: list, index: int):
         self.rect = pygame.Rect(pos[0], pos[1], *self.size)
         self.name = name
         self.game = game
+        self.state = index
+        # will be replaced
+        if (self.state == 0):
+            self.color = (0, 0, 0)
+        elif (self.state == 1):
+            self.color = (0, 0, 255)
+        elif (self.state == 2):
+            self.color = (0, 255, 0)
+        elif (self.state == 3):
+            self.color = (255, 0, 0)
 
     def fall(self):
         # Move the trash down the screen
         self.rect.y += TRASH_SPEED
-
-        # Delete the trash if it goes out of bounds
-        # if self.rect.y > SCREEN_HEIGHT:
-        #     self.game.trash_list.remove(self)
 
     def render(self):
         pygame.draw.rect(self.game.screen, self.color, self.rect)
